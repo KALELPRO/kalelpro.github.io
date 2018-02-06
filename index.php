cfae8724bb3705fef134d826a115444a6da99600
KALELPRO <thekhalilazmi@gmail.com>
2018-02-06 11:41:37 +0700

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php  
-  // halaman utama (index.php) sengaja dikosongkan
-  // karena langsung dialihkan ke file media.php
-  // cara pengalihan file bisa dilihat melalui file .htaccess yang bisa dibuka pakai Notepad
-?>
